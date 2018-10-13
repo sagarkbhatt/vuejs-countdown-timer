a6be149fbadda9f394696295f32cdd3bf6ef956f
sagarkbhatt <sagarkbhatt95@gmail.com>
2018-10-13 23:40:59 +0530

    Update timer date

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ Vue.component( 'countdown-timer', CountDownTimer );
 
 const App = Vue.extend( {
 
-	template: '<countdown-timer deadline="2018-08-21 12:00:00" ></countdown-timer>'
+	template: '<countdown-timer deadline="2020-08-21 12:00:00" ></countdown-timer>'
 
 } );
 
